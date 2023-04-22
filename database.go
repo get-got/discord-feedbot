8a348ecec4b220950be1f2e463fe556ce2f4ef08
Drn <43052079+get-got@users.noreply.github.com>
2023-04-21 21:50:35 -0700

    db work

--- database.go
+++ database.go
@@ -18,18 +18,35 @@ var (
 
 type dbRef struct {
 	gorm.Model
-	ref       string // url, link, etc
-	module    string
-	sentTo    []string // discord channels it's sent to
-	timestamp time.Time
+	ref         string // url, link, etc
+	destination string // discord channels it's sent to
+	module      string
+	timestamp   time.Time
 }
 
 func loadDatabase() error {
-	dbRefs, err := gorm.Open(sqlite.Open(pathDatabaseRefs), &gorm.Config{})
+	var err error
+	dbRefs, err = gorm.Open(sqlite.Open(pathDatabaseRefs), &gorm.Config{})
 	if err != nil {
 		return err
 	}
 	dbRefs.AutoMigrate(&dbRef{})
 
 	return nil
+}
+
+func refCheckSentAnywhere(ref string) bool {
+	var refs []dbRef
+	dbRefs.Where("ref = ?", ref).Find(&refs)
+	return len(refs) > 0
+}
+
+func refCheckSentToChannel(ref string, channel string) bool {
+	var refs []dbRef
+	dbRefs.Where("ref = ? AND destination = ?", ref, channel).Find(&refs)
+	return len(refs) > 0
+}
+
+func refLogSent(ref string, channel string, module string) {
+	dbRefs.Create(&dbRef{ref: ref, destination: channel, module: module, timestamp: time.Now()})
 }
